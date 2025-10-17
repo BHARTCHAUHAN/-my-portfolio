5699ac77265dee4f473c6a63dab88d0437a59ef8
BHARTCHAUHAN <chauhanbhart2792@gmail.com>
2025-10-17 10:02:16 +0530

    Integrated Bootstrap 5 with custom theme - enhanced buttons, cards, forms, modals with gradient styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
